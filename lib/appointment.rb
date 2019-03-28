bca5a2a75899c89d06ffcb7fa76814c2fca66d26
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-28 01:54:00 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,12 +2,17 @@ class Appointment
 
   attr_accessor :name
 
-  def initialize(name)
+  def initialize(name, patient, doctor, date = "")
     @name = name
+    @patient = patient
+    @doctor = doctor
+    @date = ""
     @@all << self
   end
 
   def self.all
     @@all
   end
+
+  
 end
